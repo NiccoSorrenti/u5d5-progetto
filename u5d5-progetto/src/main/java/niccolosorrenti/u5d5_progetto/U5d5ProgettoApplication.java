3ef5d9938dec84867cd0cf2b2d4377d96cbf8101
Niccolò <niccolo.sorrenti@gmail.com>
2026-04-10 15:30:46 +0200

    Initial commit

---
+++ u5d5-progetto/src/main/java/niccolosorrenti/u5d5_progetto/U5d5ProgettoApplication.java
@@ -0,0 +1,13 @@
+package niccolosorrenti.u5d5_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d5ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d5ProgettoApplication.class, args);
+	}
+
+}
---
+++ u5d5-progetto/src/test/java/niccolosorrenti/u5d5_progetto/U5d5ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package niccolosorrenti.u5d5_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d5ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
